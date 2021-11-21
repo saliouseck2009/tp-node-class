507e44f7a0dcb96d52da950a9bbb201978b9d6a2
saliou Seck <saliouseck2009@gmail.com>
2021-11-21 23:14:06 +0000

    fix env vars

--- services/mongoose.service.js
+++ services/mongoose.service.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 class MongooseService {
     static connectToDB(){
-        mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+'/myFirstDatabase?retryWrites=true&w=majority',
+        //mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+'/myFirstDatabase?retryWrites=true&w=majority',
+        mongoose.connect('mongodb+srv://tdsi:toor@tpclassdb.tynsu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
         { useNewUrlParser: true,
         useUnifiedTopology: true })
             .then(() => console.log('Connexion à MongoDB réussie !'))
